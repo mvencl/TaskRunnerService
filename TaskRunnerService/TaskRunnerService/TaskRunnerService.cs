946f9e351b519df0287be9db7169fdd2bed05913
mvencl <mvencl@foxconn.com>
2017-01-17 22:02:08 +0100

    Create Project

---
+++ TaskRunnerService/TaskRunnerService/TaskRunnerService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Timers;
+using System.Threading.Tasks;
+
+namespace TaskRunnerService
+{
+    public partial class TaskRunnerService : ServiceBase
+    {
+        Timer timer = null;
+        public TaskRunnerService()
+        {
+            InitializeComponent();
+        }        
+
+        protected override void OnStart(string[] args)
+        {
+            timer = new Timer(1000);
+            timer.Elapsed += OnTimedEvent;
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        private static void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            try
+            {
+
+            }   
+            catch(Exception ex)
+            {
+
+            }    
+        }
+
+        protected override void OnStop()
+        {
+            timer.Stop();
+            timer.Enabled = false;
+            timer.Dispose();          
+        }
+    }
+}
